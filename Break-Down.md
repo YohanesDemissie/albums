3700ad3f24a6b50929af21a7c8c55cd20d115e34
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-05-18 05:42:39 -0700

    purchase button fully styled, functioning!

--- Break-Down.md
+++ Break-Down.md
@@ -19,6 +19,20 @@ the curly brackets are "properties"
 all styling methods are camel case instead of hyphenated like CSS
 there are no measurement formats like px, em, etc. All one universal unit, pre-declared by ReactNative/JSX
 
+thumbnail_Images must be set to size that fits within its card to actually render to the DOM
+
+height: 300, //this is an optional number. can be any height
+    flex: 1,
+    width: null
+
+ScrollView "PArt of the react-native library necessary to scroll through content, used as an lement <ScrollView>{props.albums} </ScrollView>
+
+in **index.js the parent tag in JSX must have style {{ flex: 1}} for component to be fully accessible and responsive through scroll view
+
+</TouchableOpacity>
+user feedback on clicked links, picture, etc. that redirect the application
+
+
 #Justify Content
 great way to re arrange content within a flex-box
 
--- source/components/AlbumDetail.js
+++ source/components/AlbumDetail.js
@@ -1,13 +1,38 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, Image, Linking } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
+import Button from './Button'
 
-const AlbumDetail = (props) => {
+// const AlbumDetail = (props) => { //let deconstruct properties to minimize repition
+const AlbumDetail = ({album}) => {
+  const { title, artist, thumbnail_image, image, url } = album;
+
+  const { 
+    thumbnailContainerStyle, 
+    thumbnailStyle, 
+    headerContentStyle, 
+    headerTextStyle, 
+    imageStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image style={imageStyle} source={{uri: image}} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}/>
       </CardSection>
     </Card>
   )
@@ -15,7 +40,30 @@ const AlbumDetail = (props) => {
 
 const styles = {
   headerContentStyle: {
-    backgroundColor: '#fff'
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+
+  headerTextStyle: {
+    fontSize: 18
+  },
+
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 
