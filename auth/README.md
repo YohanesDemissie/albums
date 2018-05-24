1aea4ff406841a9de8366497adb4438f19982577
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-05-23 23:31:13 -0700

    created state for loading spinner, error message, and login form

--- auth/README.md
+++ auth/README.md
@@ -9,3 +9,6 @@ click
 #React-Native Library
 Text for raw t3xts
 TextInput for forms to act as user inputs (CAUTION: dimensions must be declared (width and height) to make visible)
+
+#Input Js 
+Responsible for the appearance of login format and styling
--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,23 +1,90 @@
 import React, { Component } from 'react'
-import { TextInput } from 'react-native'
-import { Card, CardSection, Button } from './common'
+import { Text } from 'react-native'
+import firebase from 'firebase'
+import { Card, CardSection, Button, Input } from './common'
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '',
+            password: '',
+            error: '',
+            loading: false
+          };
+
+  onButtonPress () {
+    const { email, password } = this.state
+
+    this.setState({ error: '', loading: true }) //hides error message on correct email/password input
+
+    firebase.auth().signInWithEmailAndPassword(email, password) //NOT ARBITRARY except params
+      .then(this.onLoginSuccess.bind(this)) //arbitrary naming convention
+      .catch(() => { //creating a promise to create account if incorrect login input
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this)) //binding state
+          .catch(this.onLoginFail.bind(this)); //binding state
+      });
+  }
+
+  onLoginFail() {
+    this.setState({ error: 'Authentication Failed', loading: false})
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    })
+  }
+
+  renderButton() {
+    if(this.state.loading) {
+      return <Spinner size='small' />
+    }
+    return (
+      <Button buttonText="Log In Fucker" onPress={this.onButtonPress.bind(this)} />
+    )
+  }
+
   render() {
     return(
       <Card>
         <CardSection>
-          <TextInput 
-            value = {this.state.text}
-            onChangeText = { text => this.setState ({ text })} 
-            style={{ height: 20, width: 100}} />
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            value = {this.state.email}
+            onChangeText = { email => this.setState ({ email })}
+          />
         </CardSection>
+
         <CardSection>
-          <Button buttonText="Log In Fucker" />
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="password"
+            value = { this.state.password }
+            onChangeText = { password => this.setState ({ password })}
+          />
+
+        </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
+        <CardSection>
+          {this.renderButton()}
         </CardSection>
       </Card>
     )
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
 
